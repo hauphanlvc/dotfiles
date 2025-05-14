db68cb9da336a1323f6c86b75e342654bddb9e53
Hau Phan Trong <hauphanlvc@gmail.com>
2025-05-14 22:53:06 +0700

    feat: add tab completion and wildmenu for neovim
    Signed-off-by: Hau Phan Trong <hauphanlvc@gmail.com>

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
+-- Provides tab-completion for all file-related tasks by adding '**' to the path
+vim.opt.path:append("**")
+
+-- Display all matching files when we tab complete
+vim.opt.wildmenu = true
