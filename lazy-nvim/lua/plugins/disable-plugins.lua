602febd77a43ca99154bae0eb9b42ff70236d68f
Hau Phan Trong <hauphanlvc@gmail.com>
2025-07-08 00:12:35 +0700

    chore: use neovim without any plugins
    Signed-off-by: Hau Phan Trong <hauphanlvc@gmail.com>

---
+++ lazy-nvim/lua/plugins/disable-plugins.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup()
+      vim.cmd([[colorscheme monokai-pro]])
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "default",
+    },
+  },
+}
