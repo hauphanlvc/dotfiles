0693559a70f6fef577b882015cd76ce2b20b6750
Hau Phan Trong <hauphanlvc@gmail.com>
2025-08-06 10:12:05 +0700

    chore: use jk to exit mode neovim
    Signed-off-by: Hau Phan Trong <hauphanlvc@gmail.com>

--- nvim-without-plugins/init.lua
+++ nvim-without-plugins/init.lua
@@ -6,13 +6,18 @@ vim.opt.softtabstop = -1
 vim.opt.ignorecase  = true
 vim.opt.smartcase   = true
 vim.opt.conceallevel = 0  -- Don't hide markup
--- vim.cmd("syntax off | highlight Normal guifg=#ffaf00 guibg=#282828")
+vim.cmd("colorscheme desert")
 vim.api.nvim_create_autocmd("BufEnter", {callback = function() vim.treesitter.stop() end})
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.cmd("noswapfile enew | setlocal buftype=nofile bufhidden=wipe | call feedkeys(':r !', 'n')") end)
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+vim.opt.timeout = true
+vim.opt.timeoutlen = 300
 
 vim.opt.grepprg = "rg --vimgrep --smart-case --glob '!.git/*'"
 
+vim.opt.termguicolors = true
 
 function goplsLSP() 
   vim.lsp.config('gopls', {
