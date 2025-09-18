7b9b9d1c6bd90ed5b9dfc4db3a4c29394efa22e1
Hau Phan Trong <hauphanlvc@gmail.com>
2025-09-18 10:17:39 +0700

    fix(neovim): set up gopls
    - Set up gopls language server for enable autocomple, diagnostics golang
    source code
    - Add some keymaps to support gopls
    
    Signed-off-by: Hau Phan Trong <hauphanlvc@gmail.com>

--- nvim-without-plugins/init.lua
+++ nvim-without-plugins/init.lua
@@ -8,15 +8,16 @@ vim.opt.smartcase = true
 vim.opt.conceallevel = 0 -- Don't hide markup
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
-
 vim.opt.timeout = true
 vim.opt.timeoutlen = 300
-
 vim.opt.grepprg = "rg --vimgrep --smart-case --glob '!.git/*'"
-
 vim.opt.termguicolors = true
 
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Show diagnostic" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics list" })
 -- Reference: https://neovim.io/doc/user/lsp.html#_lua-module:-vim.lsp.completion
 vim.cmd([[set completeopt+=menuone,noselect,popup]])
 -- Set up gopls
@@ -60,25 +61,40 @@ vim.lsp.config("gopls", {
 	},
 })
 
+vim.diagnostic.config({
+	underline = true,
+	update_in_insert = false,
+	virtual_text = {
+		spacing = 4,
+		source = "if_many",
+		prefix = "●",
+		-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+		-- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+		-- prefix = "icons",
+	},
+	severity_sort = true,
+})
+
 vim.lsp.enable("gopls")
 
+local on_attach = function(client, bufnr)
+	vim.lsp.completion.enable(true, client.id, bufnr, {
+		autotrigger = true,
+		convert = function(item)
+			return { abbr = item.label:gsub("%b()", "") }
+		end,
+	})
+	if client.name == "gopls" and not client.server_capabilities.semanticTokensProvider then
+		local semantic = client.config.capabilities.textDocument.semanticTokens
+		client.server_capabilities.semanticTokensProvider = {
+			full = true,
+			legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
+			range = true,
+		}
+	end
+end
 vim.lsp.start({
 	name = "gopls",
 	cmd = { "gopls" },
-	on_attach = function(client, bufnr)
-		vim.lsp.completion.enable(true, client.id, bufnr, {
-			autotrigger = true,
-			convert = function(item)
-				return { abbr = item.label:gsub("%b()", "") }
-			end,
-		})
-		if not client.server_capabilities.semanticTokensProvider then
-			local semantic = client.config.capabilities.textDocument.semanticTokens
-			client.server_capabilities.semanticTokensProvider = {
-				full = true,
-				legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
-				range = true,
-			}
-		end
-	end,
+	on_attach = on_attach,
 })
